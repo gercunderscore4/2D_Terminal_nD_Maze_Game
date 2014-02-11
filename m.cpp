ac9ce1c653df97c8faf46567e8134318a8a03c84
gercunderscore4 <gercunderscore4@gmail.com>
2014-02-10 17:56:27 -0800

    minor tweaks

--- m.cpp
+++ m.cpp
@@ -2,7 +2,7 @@
  * FILE:    m.cpp
  * PURPOSE: 4D maze game
  * AUTHOR:  Geoffrey Card
- * DATE:    2014-02-06 - 2014-02-06
+ * DATE:    2014-02-06 - 2014-02-10
  * NOTES:   Controls are un-intuitive.
  *          Graphics are too big.
  */
@@ -10,6 +10,10 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include "mn.hpp"
+#include "m1.hpp"
+#include "m2.hpp"
+#include "m3.hpp"
 #include "m4.hpp"
 
 int main (void)
@@ -18,13 +22,13 @@ int main (void)
 	srand((int) time(NULL));
 	
 	// clear screen
-	for (int i = 0; i < 200; i++) {
+	for (int i = 0; i < CLR_LEN; i++) {
 		printf("\n");
 	}
 	
 	// title screen
 	printf(
-		"2D Terminal 4D Maze Game\n"
+		"2D Terminal nD Maze Game\n"
 		"                        \n"
 		"   By Geoffrey Card     \n"
 		"                        \n"
@@ -36,13 +40,11 @@ int main (void)
 	do {
 		c = getchar();
 	} while (c != '\n');
-	for (int i = 0; i < 200; i++) {
-		printf("\n");
-	}
+
+	// game
 	m4 maze;
-	maze.test();
+	maze.play();
 	
 	//printf("\n\nGood-bye, cruel world!\n");
 	return 0;
 }
-
