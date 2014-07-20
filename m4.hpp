21e0a93cb4783023eee4dab82c4a42e99b2ae17b
Geoffrey Card <gercunderscore4@gmail.com>
2014-07-20 01:37:22 -0300

    Algorithm selection and discovery (incomplete)

--- m4.hpp
+++ m4.hpp
@@ -20,40 +20,38 @@ typedef struct prim {
 
 class m4 {
 	public:
+		//////////////// CLASS ////////////////
+		m4 (void);
+		~m4 (void);
+		
+		//////////////// META ////////////////
+		void do_stuff (void);
+		void play (void);
+		void play (int xs, int ys, int zs, int ws, int algs);
+		void test (int n, int xs, int ys, int zs, int ws);
+		void tutorial (void);
+		
+	private:
+		//////////////// VARIABLES ////////////////
 		// maze dimensions
 		int lenx, leny, lenz, lenw;
-
 		// maze data
 		// total size is [lenx][leny][lenz][lenw]
 		node_t**** arry;
-	
 		// user location
 		int x,y,z,w;
-	
 		// goal coordinates
 		int gx,gy,gz,gw;
-		
+		// maze algorithm
+		int alg;
 		// axes
 		int a0, a1, a2, a3;
 
-		// constructor
-		m4 (void);
-		// destructor
-		~m4 (void);
-		
-		// test stuff here
-		void do_stuff (void);
-		void play (void);
-		void play (int xs, int ys, int zs, int ws);
-		void test (int n, int xs, int ys, int zs, int ws);
-		void tutorial (void);
-
-		// allocate and deallocate maze data
+		//////////////// ALLOCATION ////////////////
 		void gen (void);
 		void degen (void);
 		
-		// contructicon
-		// these may destroy any non-wall flags
+		//////////////// BOXES ////////////////
 		// closes off maze, does not interfere with internal walls
 		void frame (void);
 		// open space inside
@@ -63,25 +61,33 @@ class m4 {
 		// empty
 		void empty (void);
 		
-		// print
+		//////////////// GRAPHICS ////////////////
 		void print_clr (void);
 		void print_title_screen (void);
-		void print_all (void);
 		void print_man (void);
+		void print_all (void);
+		void print_disc (void);
 		void print_data (void);
-		
+		void disc_line_of_sight (void);
+		void disc_line_of_sight_ranged (int range);
+		void disc_ranged (int range);
+
+		//////////////// CONTROLS ////////////////
 		// user input
 		void get_size (void);
+		void build_maze(int algs);
 		bool control (void);
 		// dimension swap
 		void d_swap_abs (int d1, int d2);
 		void d_swap_rel (int d1, int d2);
 
+		//////////////// CHECKS ////////////////
 		// checks
 		bool inline valid (void);
 		bool inline valid (int i, int j, int k, int h);
 		void inline set_flag (node_t flag);
 		void inline set_flag (int i, int j, int k, int h, node_t flag);
+		void inline set_flag_all (node_t flag);
 		void inline clear_flag (node_t flag);
 		void inline clear_flag (int i, int j, int k, int h, node_t flag);
 		void inline clear_flag_all (node_t flag);
@@ -94,13 +100,17 @@ class m4 {
 		bool smash (char dir);
 		bool smash (int i, int j, int k, int h, char dir);
 
+		//////////////// SOLVERS ////////////////
 		// solvers
+		int depth_solve (void);
 		int rec_depth_solve (void);
 		int breadth_solve (void);
 		
+		//////////////// BUILDERS ////////////////
 		// maze algorithms
 		// random, returns solvability
 		void random_build (void);
+		void random_solved_build (void);
 		void depth_build (void);
 		void rec_depth_build (void);
 		void breadth_build (void);
