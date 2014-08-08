819eeeee27f87200bd5c526f4afbcf716f4ac439
Geoffrey Card <gercunderscore4@gmail.com>
2014-08-08 15:24:14 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,189 @@
+#include <cstdio>
+#include <cmath>
+#include "SDL2/SDL.h"
+#include "m4.h"
+#include "fileio.h"
+#include "display.h"
+
+/*
+#if defined(__IPHONEOS__) || defined(__ANDROID__)
+	#include "SDL_opengles.h"
+#else // Computer
+	// openGL
+	#include "SDL2/SDL_opengl.h"
+#endif
+*/
+
+using namespace std;
+
+#define MAZEX 3
+#define MAZEY 3
+#define MAZEZ 3
+#define MAZEW 3
+
+int main (void)
+{
+	char buff[FILE_BUFFER_SIZE];
+	sprintf(buff, "\n");
+	writeToFile(buff);
+	
+	sprintf(buff,"BEGIN PROGRAM");
+	writeToFile(buff);
+		
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		sprintf(buff, "ERROR: Failed to init. %s\n", SDL_GetError());
+		writeToFile(buff);
+		return 1;
+	}
+
+	SDL_DisplayMode dm;
+	if (SDL_GetDesktopDisplayMode(0, &dm) != 0) {
+		sprintf(buff, "ERROR: Desktop side unknown. %s\n", SDL_GetError());
+		writeToFile(buff);
+		SDL_Quit();
+		return 1;
+	}
+		
+	// create window
+	SDL_Window* window = NULL;
+	window = SDL_CreateWindow("gc", 0, 0, dm.w, dm.h, SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN); 
+	if (window == NULL) {
+		sprintf(buff, "ERROR: Window did not create. %s\n", SDL_GetError());
+		writeToFile(buff);
+		SDL_Quit();
+		return 1;
+	}
+	
+	SDL_Renderer* renderer = NULL;
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == NULL) {
+		sprintf(buff, "ERROR: Renderer did not create. %s\n", SDL_GetError());
+		writeToFile(buff);
+		SDL_Quit();
+		return 1;
+	}
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	
+	// init game stuff
+	sprintf(buff,"MAKE MAZE");
+	writeToFile(buff);
+	m4 maze;
+	maze.makeMaze(MAZEX, MAZEY, MAZEZ, MAZEW);
+	
+	sprintf(buff,"BEGIN LOOP");
+	writeToFile(buff);
+
+	bool quit = false;
+	SDL_Event event;
+	// while game not quit, continue playing game
+	while (quit == false) {
+		
+		sprintf(buff, "LOOPING");
+		writeToFile(buff);
+		
+		// event loop (while stuff is happening, do that stuff
+		while (SDL_PollEvent(&event) != 0) {
+
+			// parse events
+			sprintf(buff, "EVENT");
+			writeToFile(buff);
+	
+
+			
+			// KEYBOARD CONTROLS			
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			} else if (event.type == SDL_KEYDOWN) {
+				switch (event.key.keysym.sym) {
+					case SDLK_BACKQUOTE:
+					{
+						sprintf(buff, "QUIT");
+						writeToFile(buff);
+						quit = true;
+						break;
+					}
+					case SDLK_a:
+					{
+						maze.move(XD);
+						break;
+					}
+					case SDLK_d:
+					{
+						maze.move(XU);
+						break;
+					}
+					case SDLK_w:
+					{
+						maze.move(YD);
+						break;
+					}
+					case SDLK_s:
+					{
+						maze.move(YU);
+						break;
+					}
+					case SDLK_q:
+					{
+						maze.move(ZD);
+						break;
+					}
+					case SDLK_e:
+					{
+						maze.move(ZU);
+						break;
+					}
+					case SDLK_z:
+					{
+						maze.move(WD);
+						break;
+					}
+					case SDLK_c:
+					{
+						maze.move(WU);
+						break;
+					}
+				}
+			}
+			maze.discover();
+			if (maze.get_flag(F_GOAL) == true ) {
+				sprintf(buff, "WIN");
+				writeToFile(buff);
+				maze.makeMaze(MAZEX, MAZEY, MAZEZ, MAZEW);
+				//quit = true;
+			}
+		}
+
+		sprintf(buff, "GRAPHICS");
+		writeToFile(buff);
+
+
+		// GRAPHICS
+		// clear screen
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+		SDL_RenderClear(renderer);
+	
+		draw_disc_better(&maze, renderer);
+
+		// update screen
+		SDL_RenderPresent(renderer);
+	}
+	
+	sprintf(buff,"END LOOP");
+	writeToFile(buff);
+
+	sprintf(buff, "END PROGRAM");
+	writeToFile(buff);
+	
+	sprintf(buff, "\n");
+	writeToFile(buff);
+	
+	// destroy window
+	// quit SDL subsystems
+	SDL_DestroyRenderer(renderer);
+	renderer = NULL;
+	SDL_DestroyWindow(window);
+	window = NULL;
+	SDL_Quit();
+
+	return 0;
+}
