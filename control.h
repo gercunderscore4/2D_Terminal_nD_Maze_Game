55c3bbde6def7615cf9de717504886cbbba522fa
Geoffrey Card <gercunderscore4@gmail.com>
2014-08-08 16:06:31 -0400

    Create control.h

---
+++ control.h
@@ -0,0 +1,11 @@
+#ifndef CONTROL_H_
+#define CONTROL_H_
+
+#include "SDL2/SDL.h"
+#include "m4.h"
+
+bool controls_k (m4* maze, SDL_Event* event);
+
+bool controls_t (m4* maze, SDL_Event* event);
+
+#endif // CONTROL_H_
